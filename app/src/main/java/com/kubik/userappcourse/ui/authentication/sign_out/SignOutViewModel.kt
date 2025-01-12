87fa4fa7b2b43d6689b50e70e863dfdd4fe8e4c6
kubik <kostaplay9@mail.ru>
2025-01-12 20:37:22 +0300

    SignOut переписан на StateFlow

--- app/src/main/java/com/kubik/userappcourse/ui/authentication/sign_out/SignOutViewModel.kt
+++ app/src/main/java/com/kubik/userappcourse/ui/authentication/sign_out/SignOutViewModel.kt
@@ -2,37 +2,33 @@ package com.kubik.userappcourse.ui.authentication.sign_out
 
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kubik.userappcourse.domain.user.SignUpUserUseCase
 import com.kubik.userappcourse.domain.user.UserRepository
 import com.kubik.userappcourse.ui.authentication.models.UserModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class SignOutViewModel : ViewModel() {
 
-    val isSuccessfulSignUp = MutableLiveData<Boolean>()
-    val exitsLogin = MutableLiveData<Int>(0)
+    private val _isSuccessfulSignUp = MutableStateFlow<Boolean?>(null)
+    val isSuccessfulSignUp = _isSuccessfulSignUp.asStateFlow()
+    private val _exitsLogin = MutableStateFlow<Int>(0)
+    val exitsLogin = _exitsLogin.asStateFlow()
 
     fun registerUser(user: UserModel, userRepository: UserRepository) {
         viewModelScope.launch(Dispatchers.IO) {
             SignUpUserUseCase(userRepository).signUp(
                 user = user.toUserDomainModel(),
                 resultUserSignIn = {
-                    Handler(Looper.getMainLooper()).post{
-                        isSuccessfulSignUp.value = it
-                    }
+                    _isSuccessfulSignUp.value = it
                 },
                 loginExists = {
-                    Handler(Looper.getMainLooper()).post{
-                        try {
-                            exitsLogin.value = exitsLogin.value!! + 1
-                        } catch (e: ExceptionInInitializerError) {
-                            Log.e("MyLog", e.message.toString())
-                        }
+                    Handler(Looper.getMainLooper()).post {
+                        _exitsLogin.value += 1
                     }
                 }
             )
